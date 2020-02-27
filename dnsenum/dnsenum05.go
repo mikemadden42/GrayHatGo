309182f0a73e56eb854a1b00099f4aaa30b6ae3c
Michael Madden <mikemadden42@gmail.com>
2020-02-26 21:22:34 -0600

    Create functions to resolve single host and subdomains.

---
+++ dnsenum/dnsenum05.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"sync"
+)
+
+var wg sync.WaitGroup
+
+func main() {
+	// The list of domains was fetched from https://github.com/bitquark/dnspop.
+	subs := subdomains("subdomains.txt")
+	domain := flag.String("domain", "microsoft.com", "destination domain name")
+	debug := flag.Bool("debug", false, "debug mode")
+
+	flag.Parse()
+	if flag.NFlag() == 0 {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	resolveSubDomains(*domain, subs, *debug)
+}
+
+func subdomains(file string) []string {
+	domains, err := os.Open(file)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer domains.Close()
+
+	scanner := bufio.NewScanner(domains)
+	subs := make([]string, 0)
+	for scanner.Scan() {
+		subs = append(subs, scanner.Text())
+	}
+	return subs
+}
+
+func resolveHost(host string, debug bool) {
+	ips, err := net.LookupIP(host)
+	if err == nil {
+		fmt.Println(host)
+		if debug {
+			for _, ip := range ips {
+				fmt.Println(ip)
+			}
+			fmt.Println()
+		}
+	}
+}
+
+func resolveSubDomains(host string, subs []string, debug bool) {
+	for _, sub := range subs {
+		resolveHost(sub+"."+host, debug)
+	}
+}
